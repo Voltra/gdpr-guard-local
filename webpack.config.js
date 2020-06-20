ccc2cad903c7f5621ff04518ce7397c71830e6c7
Voltra <ludwig.guerin.98@gmx.fr>
2020-06-20 17:08:04 +0200

    Add config files and readme

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+/**
+ * Create an absolute URL to the given URI inside the current directory
+ * @param {string} [uri = ""] - The URI to the desired file
+ * @returns {string}
+ */
+const here = (uri = "") => path.resolve(__dirname, uri);
+
+module.exports = {
+	mode: "production",
+	entry: here("./src/index.ts"),
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /(?!\.d)\.tsx?$/i,
+				use: "ts-loader",
+				include: here("src/"),
+				exclude: [
+					"node_modules/",
+					"test/"
+				].map(here),
+			},
+		],
+	},
+	resolve: {
+		extensions: ["ts", "js", "tsx"].map(ext => `.${ext}`),
+	},
+	output: {
+		filename: "index.js",
+		path: here("dist"),
+		library: "gdprGuardLocal",
+		libraryTarget: "umd",
+	},
+};
